cd2114f7115e388f64a523c39be6f9a8826eb104
denis82 <deniskontek@gmail.com>
2021-12-23 17:43:00 +0100

    Moja strona

---
+++ scripts/edycja.js
@@ -0,0 +1,42 @@
+function zmien1() {
+
+document.getElementById("h1").innerHTML="O Mnie";
+    
+document.getElementById("h2").innerHTML="Witaj nazywam sie Denis Rudolf Damian Kontek jestem informatykiem moje hobby to piłka nożna programowanie atletyka  jestem z Polski z Woj.Śląskiego z miasta Gliwice urodziłem sie 26 maja 2003 roku ";
+    
+}
+function zmien2() {
+    
+document.getElementById("h1").innerHTML="Oferta";
+    
+document.getElementById("h2").innerHTML="Pisze w językach C/C++/C#,Python,HTML,CSS,JS";
+    
+}
+function zmien3() {
+    
+document.getElementById("h1").innerHTML="Wiadomość";
+    
+document.getElementById("h2").innerHTML="Witaj mam nadzieje że spodobała ci sie moja strona :)";
+    
+}
+function zmien4() {
+    
+document.getElementById("h1").innerHTML="O Stronie";
+    
+document.getElementById("h2").innerHTML="Strona została napisana przezemnie w językach HTML CSS i JS pisałem w IDE o nazwie Brackets";
+    
+}
+function zmien5() {
+    
+document.getElementById("h1").innerHTML="Kontakt";
+    
+document.getElementById("h2").innerHTML="Kontakt do mnie może być poprzez Social media które są na dole lub poprzez email: deniskontek@gmail.com";
+    
+}
+function zmien6() {
+    
+document.getElementById("h1").innerHTML="Info";
+    
+document.getElementById("h2").innerHTML="Autor: Denis Kontek - denis82 " + " " + " Data ostatniej edycji strony:20.07.2019";
+    
+}
